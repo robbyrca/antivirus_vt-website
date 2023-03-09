6a1b67fd2d4a95fd86c1907a40cf9213ff53f2e0
robbyrca <rcuenca.alt@gmail.com>
2023-03-09 16:20:40 +0100

    backup from server

--- test.php
+++ test.php
@@ -8,20 +8,24 @@
     //Definir cte
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
-
-    //conexio servidor bd
-    $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-    echo ("1");
+    try{
+        //conexio servidor bd
+        $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+    
     //variable del formulario
     $log = $_REQUEST['login'];
     $pas = md5($_REQUEST['pass']);
-    echo ("2");
+    
     //eliminar igual del camp pass
     $log = str_replace("=", "", $log);
     $log = str_replace("'", "", $log);
     $pas = str_replace("=", "", $pas);
     $pas = str_replace("'", "", $pas);
-    echo ("3");
+
     if (isset($_REQUEST['volver'])){
         echo "<br>1. <a href='alta.php'>Alta</a>";
         echo "<br>2. <a href='baja.php'>Baja</a>";
@@ -29,7 +33,7 @@
         echo "<br>4. <a href='modifica2.php'>Modifica</a>";
         echo "<br><br><form><a href='login_form.html'/><input type='button' value='SORTIR'></a>";
     }
-    echo ("4");
+
     if (isset($_REQUEST['volver2'])){
         header('Location: login.php');
     }
@@ -37,50 +41,48 @@
     if(isset($_REQUEST['volver3'])){
         header("Location: alta.php");
     }
-    echo ("5");
     //comprovem si venim del buto enviar
     if (isset($_REQUEST['enviar'])){
         //prepar query
-        $sql="select * from usuarios where user='".$log."' and pass='".$pas."'";
-
-        //echo $sql;
+        $sql = "select * from usuarios where user='".$log."' and pass='".$pas."'";
+        //$sql="select * from usuarios where user";
        // $consulta = $db->prepare($sql);
        //preparar la execucio
        //$consulta = mysql_query ($sql, $db);
-
+       
       //mostrar num de registres
        //$nfilas = mysql_num_rows($consulta);
-        $count = $db->exec($sql);
-        echo $count;
       //comprobar si existeixen registres
-      if ($count!=0){
+      $nfilas=0;
+    foreach($db->query($sql) as $result){
+        if ($result) $nfilas++;
+    }
+    $dbh = null;
+      print  ($nfilas);
+      print_r($result[user]);
+      if ($nfilas!=0){
           //echo "Usuario v&aacute;lido";
           echo "<br>1. <a href='alta.php'>Alta</a>";
           echo "<br>2. <a href='baja.php'>Baja</a>";
           echo "<br>3. <a href='consulta.php'>Consulta</a>";
           echo "<br>4. <a href='modifica2.php'>Modifica</a>";
-          echo "<br><br><form><a href='login_form.html'/><input type='button' value='SORTIR'></a>";
+          echo "<br><br><form><a href='login.php'/><input type='button' value='SORTIR'></a>";
       }else{
-            echo "Usuario no v&aacute;lido";
+            echo " Usuario no v&aacute;lido";
         }
     }
-    echo ("6");
+    
     //comprovem si venim del buto alta
     if (isset($_REQUEST['alta'])){
         //prepar query
         $sql="insert into usuarios (user, pass) values ('".$log."','".$pas."');";
-        echo("7.1");
-        echo($sql);
         //confirmacion de alta
         //$consulta = PDO::query ($sql, $db);
-        $consulta = $db->prepare($sql);
-        echo("7.2"); 
+        $db->query($sql);
         //echo $sql;
-        $consulta->execute();
         echo "Usuario dado de ALTA";
         echo "<br><form action='test.php' method='post'><input type='submit' name='volver2' value='TORNAR'>";
-        echo("7.3");
-        mail($log,'Email ALTA','Bienvenido/a '.$log);
+        #mail($log,'Email ALTA','Bienvenido/a '.$log);
 
     }
     if (isset($_REQUEST['alta2'])){
