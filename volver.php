c7196385e6707dc655da7972e2e01877f24bbcb8
robbyrca <116655222+robbyrca@users.noreply.github.com>
2023-05-18 15:40:11 +0200

    Delete volver.php

--- volver.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<br>1. <a href='alta.php'>Alta</a>";
-echo "<br>2. <a href='baja.php'>Baja</a>";
-echo "<br>3. <a href='consulta.php'>Consulta</a>";
-echo "<br>4. <a href='modifica2.php'>Modifica</a>";
-echo "<br><br><form><a href='login_form.html'/><input type='button' value='SORTIR'></a>";
-?>
